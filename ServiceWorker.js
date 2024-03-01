ebe1f26b07cf80570fc106f68c4add732e0c65be
k-taro56 <k-taro56@users.noreply.github.com>
2024-03-01 12:43:45 +0000

    deploy: AntiInsufficientSleep/Cosmic-Voyagers@91ee08034175f7163b96361e4f86bfc1915f6b67

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "徹夜はするな。睡眠不足は良い仕事の敵だ。-Cosmic Voyagers-0.0.445";
+const cacheName = "徹夜はするな。睡眠不足は良い仕事の敵だ。-Cosmic Voyagers-0.0.449";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js",
